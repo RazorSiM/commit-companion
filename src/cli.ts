737dec8985ff8fbfb0456aaccc9f88e168749cb7
Raz <me@raz.wtf>
2024-03-25 18:25:24 +0100

    update models

--- src/cli.ts
+++ src/cli.ts
@@ -3,16 +3,16 @@
 import { defineCommand, runMain } from "citty";
 
 export const main = defineCommand({
-  meta: {
-    name: "commit message companion",
-    description: "I will help you write beautiful commit messages through AI!",
-    version: "0.0.1",
-  },
-  subCommands: {
-    setup: () => import("./commands/setup").then((r) => r.setupCommand),
-    generate: () =>
-      import("./commands/generateCommitMessage").then((r) => r.generateCommand),
-  },
+	meta: {
+		name: "commit message companion",
+		description: "I will help you write beautiful commit messages through AI!",
+		version: "0.0.1",
+	},
+	subCommands: {
+		setup: () => import("./commands/setup").then((r) => r.setupCommand),
+		generate: () =>
+			import("./commands/generateCommitMessage").then((r) => r.generateCommand),
+	},
 });
 
 runMain(main);
